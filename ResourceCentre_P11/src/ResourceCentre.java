24c5a34f112ef753acd885e46d2a78393d97c9e7
19013309 <19013309@LAPTOP-LJ9Q2JM8>
2020-08-04 12:34:26 +0800

    ask_for_assetTag and ask_for_description added

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -24,7 +24,7 @@ public class ResourceCentre {
 		int option = OPTION_START;
 
 		while (option != OPTION_QUIT) {
-			//123
+			//1233
 			ResourceCentre.menu();
 			option = Helper.readInt("Enter an option > ");
 
